d0ecfecd58ec1f8ad8a00e2d515d2ca32e8f104d
ArielSanchez12 <riveraariel433@gmail.com>
2025-10-15 16:25:19 -0500

    Ya sirve para responderle a cada cliente en especifico, solo tocaria reemplazar las IP's por un nombre real

--- hiloCliente.java
+++ hiloCliente.java
@@ -1,40 +1,48 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.Scanner;
 
 //Este hilo se va a ejecutar en el servidor para ejecutar varios clientes
 public class hiloCliente extends Thread {
   private DatagramSocket socket;
-  private DatagramPacket datagrama;
+  private static DatagramPacket ultimoDatagrama;
 
   //constructor
   public hiloCliente(DatagramSocket socket, DatagramPacket datagrama){
     this.socket = socket;
-    this.datagrama = datagrama;
+    ultimoDatagrama = datagrama;
   }
   
   @Override
   public void run(){
     //extraer la informacion del datagrama recibido
-    String mensaje = new String(datagrama.getData(),0, datagrama.getLength()); //ni idea para que es el offset pero sin eso no funciona (no hay que escribir offset, solo escribir 0)
-    System.out.println("Mensaje recibido: " + mensaje); //el offset de arriba segun ayuda a que si se reciba el mensaje, de no estar, no va a recibir el mensaje por alguna razon
+    String mensaje = new String(ultimoDatagrama.getData(),0, ultimoDatagrama.getLength()); //ni idea para que es el offset pero sin eso no funciona (no hay que escribir offset, solo escribir 0)
+    System.out.println("Mensaje recibido: " + ultimoDatagrama.getAddress() + ":" + ultimoDatagrama.getPort()+ ": " + mensaje); //el offset de arriba segun ayuda a que si se reciba el mensaje, de no estar, no va a recibir el mensaje por alguna razon
     
     //Una vez recibimos el mensaje, le vamos a responder al cliente
     //para eso primero necesitamos obtener la IP del cliente
-    InetAddress ip_cliente = datagrama.getAddress();
-    int puerto_cliente = datagrama.getPort();
+    
 
     //mensaje de respuesta (para enviar el msj toca ponerlo dentro de un datagrama (si, todo es por medio de datagramas))
-    String respuesta = "Hola, desde el servidor";
-    //Arreglo de bytes para enviar datos
-    byte[] bufferSalida = respuesta.getBytes(); //le asignamos directamente la longitud en bytes que tenga el mensaje ya que es un mensaje quemado
-    //Crear datagrama para enviar el mensaje
-    DatagramPacket datagrama_salida = new DatagramPacket(bufferSalida, bufferSalida.length, ip_cliente, puerto_cliente); //aqui tiene que ir, el buffer, longitud del buffer, ip del cliente y su puerto
+    while (true) { 
+      Scanner input = new Scanner(System.in);
+      System.out.println("Ingresa la respuesta para el cliente: " + ultimoDatagrama.getAddress() + ":" + ultimoDatagrama.getPort()+ ": " );
+      String respuesta = input.nextLine();
 
-    try {
+      InetAddress ip_cliente = ultimoDatagrama.getAddress(); //direccion
+      int puerto_cliente = ultimoDatagrama.getPort(); // y puerto se obtienen en automatico cuando recibo mensaje siendo el servidor
+
+      //Arreglo de bytes para enviar datos
+      byte[] bufferSalida = respuesta.getBytes(); //le asignamos directamente la longitud en bytes que tenga el mensaje ya que es un mensaje quemado
+      //Crear datagrama para enviar el mensaje
+      DatagramPacket datagrama_salida = new DatagramPacket(bufferSalida, bufferSalida.length, ip_cliente, puerto_cliente); //aqui tiene que ir, el buffer, longitud del buffer, ip del cliente y su puerto
+      try {
         socket.send(datagrama_salida);
-    } catch (Exception e) {
-      e.printStackTrace();
+        System.out.println("Respuesta enviada a " + ip_cliente + ":" + puerto_cliente);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     }
   }
   
--- servidor.java
+++ servidor.java
@@ -2,6 +2,7 @@ import java.net.DatagramSocket;
 import java.net.DatagramPacket;
 //Vamos a usar hilos para esta activdad y el metodo UDP
 public class servidor{
+  private static DatagramPacket ultimoClienteDatagrama;
   public static void main(String[] args){
     int puerto = 4000; //definimos el puerto estatico (el sevidor no puede cambiar de puerto)
     //crear el socket UDP
@@ -16,7 +17,14 @@ public class servidor{
           //El servidor va a estar esperando el mensaje del cliente
           socket.receive(datagrama);
 
-          hiloCliente hilo = new hiloCliente(socket, datagrama); //pasamos el socket y datagrama para que sepa donde establecemos la conexion
+
+          // Actualizar el último cliente que escribió
+          ultimoClienteDatagrama = datagrama;
+          
+          // Mostrar de qué cliente viene el mensaje
+          System.out.println("Mensaje recibido de " + datagrama.getAddress() + ":" + datagrama.getPort());
+          
+          hiloCliente hilo = new hiloCliente(socket, ultimoClienteDatagrama);
           hilo.start();
       }
     } catch (Exception e) { //para que tome todas las excepciones le borramos lo del socket
